417034b97d5e6e48488a035a589644ca689c1f4d
LAPTOP-6A2IC92J\rdhd1 <junova.kristyna@gmail.com>
2023-03-21 21:01:51 +0100

    ukol 3

--- index.js
+++ index.js
@@ -1,10 +1,20 @@
-const formular = document.querySelector('form')
+const form = document.querySelector('form')
+const textPole = document.querySelector('input')
+
 
 const odebirat = (event) => {
 	event.preventDefault()
 	const input = document.querySelector('input')
 	const email = input.value
-	formular.textContent = `Děkujeme za váš zájem. Těšte se na novinky ze světa frontendu a UX na vaší adrese ${email}.`
+	form.textContent = `Děkujeme za váš zájem. Těšte se na novinky ze světa frontendu a UX na vaší adrese ${email}.`
 }
 
-formular.addEventListener('submit', odebirat)
+const kontrolaText = (event) => {
+	if (textPole.value === '' || textPole.value.indexOf('@') === -1) {
+		textPole.classList.add('text-pole')
+	} else {
+		textPole.classList.remove('text-pole')
+	}
+}
+form.addEventListener('submit', odebirat)
+textPole.addEventListener('input', kontrolaText)
